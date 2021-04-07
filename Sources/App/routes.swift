1592ded6b5e1e659828558e961f8400a7ef0779c
Charles Wright <cvwright@kombucha.systems>
2021-04-07 15:49:42 -0500

    Use the new modularized version of the registration handler

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,7 +6,7 @@ import Vapor
 func routes(_ app: Application, reg: RegistrationController) throws {
        
     app.post(["_matrix", "client", ":version", "register"],
-             use: reg.handleRegister)
+             use: reg.handleRegisterRequest)
         
 
         
