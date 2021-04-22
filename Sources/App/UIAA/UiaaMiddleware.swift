d00f7ab811716cf8f189b6c64f093ffa5287b43d
Charles Wright <cvwright@kombucha.systems>
2021-04-22 13:22:42 -0500

    Updated a bunch of calls to 'print' to instead use Vapor's logger

--- Sources/App/UIAA/UiaaMiddleware.swift
+++ Sources/App/UIAA/UiaaMiddleware.swift
@@ -20,48 +20,48 @@ public final class UiaaMiddleware: Middleware {
         
         // Now we look for the session ID in the request
         if let uiaaReqData = try? request.content.decode(MinimalUiaaRequestData.self) {
-            print("UIAA\tGot UIAA request data")
+            request.logger.debug("UIAA\tGot UIAA request data")
             let sessionId = uiaaReqData.auth.session
             let id = SessionID(string: sessionId)
             return self.driver.readSession(id, for: request).flatMap { data in
                 guard let data = data else {
                     // First time we're seeing this one
                     // FIXME Shouldn't this be an error?  The first time we see a new session ID, it should be coming from the *homeserver*, not the client.  Hmmm...
-                    print("UIAA\tFound a new unexpected session: \(sessionId)")
+                    request.logger.debug("UIAA\tFound a new unexpected session: \(sessionId)")
                     return request.eventLoop.makeFailedFuture(Abort(.badRequest, reason: "Unknown session \(sessionId)"))
                 }
                 
                 // We've seen this session before
-                print("UIAA\tFound an existing session")
+                request.logger.debug("UIAA\tFound an existing session")
                 // Attach its existing data to it
                 request.uiaaSession = .init(id: id, data: data)
 
                 return next.respond(to: request).map { response in
                     // Check whether we need to remove the session
-                    print("UIAA\tChecking for existing session in response")
+                    request.logger.debug("UIAA\tChecking for existing session in response")
                     return response
                 }
             }
         } else {
             // No credentials.  That's OK.
-            print("UIAA\tNo session in the request")
+            request.logger.debug("UIAA\tNo session in the request")
             return next.respond(to: request).flatMap { response in
                 // Maybe we got a new session ID here in the response?
-                print("UIAA\tChecking for new session in response")
+                request.logger.debug("UIAA\tChecking for new session in response")
                 if let initialState = try? response.content.decode(UiaaSessionState.self) {
-                    print("UIAA\tFound new session in response: \(initialState.session)")
+                    request.logger.debug("UIAA\tFound new session in response: \(initialState.session)")
                     var data = UiaaSessionData(initialData: [:], initialState: initialState)
                     data["session"] = initialState.session
                     
                     return self.driver
                         .createSession(data, for: request)
                         .map { _ in
-                            print("UIAA\tCreated new session")
+                            request.logger.debug("UIAA\tCreated new session")
                             return response
                         }
 
                 } else {
-                    print("UIAA\tNo new session in response")
+                    request.logger.debug("UIAA\tNo new session in response")
                     return request.eventLoop.makeSucceededFuture(response)
                 }
             }
