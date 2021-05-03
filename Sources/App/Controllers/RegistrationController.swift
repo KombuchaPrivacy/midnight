4bb12248f0202655f98b45d90d652abc414900b4
Charles Wright <cvwright@kombucha.systems>
2021-05-03 09:43:30 -0500

    Require that clients complete the token auth stage before attempting other UIAA stages with the homeserver.  This prevents a clever adversary from doing an end-run around the token auth stage.

--- Sources/App/Controllers/RegistrationController.swift
+++ Sources/App/Controllers/RegistrationController.swift
@@ -244,37 +244,14 @@ struct RegistrationController {
             }
             return handleSignupTokenRequest(req: req, token: token)
         default:
-            // FIXME There's a possible TOCTTOU vulnerability lurking here
-            // We've already validated the username when the first request
-            // came in without a UIAA session.
-            // BUT What's to say that the client didn't change things up and
-            // submit a *different* username for this request?
-            // If we just stupidly pass the request along to the homeserver here,
-            // then the client could sneak one by us.  Like this:
-            // Request 1:
-            //   * No UIAA session
-            //   * Username: something innocuous that doesn't trip our filters, e.g. "abcdefgh"
-            // Request 2:
-            //   * UIAA session from homeserver
-            //   * Type: signup_token
-            //   * Token: some valid token
-            //   * Username: same as in request 1, e.g. "abcdefgh"
-            // Request 3:
-            //   * UIAA session from homeserver
-            //   * Type: some type that we don't handle
-            //   * Username: something offensive that would have tripped our filters on Request 1
-            // ARGH
-            // So we need to do two things:
-            // 1. When we first create the UIAA session, store the requested
-            //    username in the UIAA session state data.
-            // 2. When we get to this point of handing off requests to the
-            //    homeserver, check that the username hasn't been changed.
-            //    Otherwise, return a 403.
-            // We should probably do something similar with the password, but
-            // it's not a good idea to be storing that in plain text.
-            // Simple hashing is not great, but it's a lot better than keeping
-            // a copy hanging around in plain text.  Maybe we could do a simple salt.
             req.logger.debug("CHUCKIE\tWe don't handle requests of type \(data.auth.type)")
+            guard let session = req.uiaaSession,
+                  let completed = session.data.state.completed,
+                  completed.contains(LOGIN_STAGE_SIGNUP_TOKEN) else {
+                req.logger.info("CHUCKIE\tClient attempted \(data.auth.type) before completing token auth")
+                let err = ResponseErrorContent(errcode: "TOKEN_STAGE_INCOMPLETE", error: "Must complete token auth before attempting other stages")
+                return err.encodeResponse(status: .forbidden, for: req)
+            }
             return proxyRequestToHomeserver(req: req).flatMap { hsResponse in
                 return handleUiaaResponse(res: hsResponse, for: req)
             }
@@ -346,7 +323,7 @@ struct RegistrationController {
             // 2) The other role for the signup token is to act as a stand-in
             //    for an Apple (or one day, Google) subscription for paid services.
             //    For a subscription service, there's no point in asking for
-            //    someone's username/password if they haven't already `paid.
+            //    someone's username/password if they haven't already paid.
             /*
             if flow.stages == ["m.login.dummy"] {
                 flow.stages = [LOGIN_STAGE_SIGNUP_TOKEN]
@@ -362,6 +339,22 @@ struct RegistrationController {
             req.logger.debug("CHUCKIE\tCompleted = \(ourResponseData.completed ?? [])")
         }
         //req.logger.debug("CHUCKIE\t\(#function): Returning response data = \(ourResponseData)")
+        
+        // FIXME Move this to the UIAA Middleware..
+        /*
+        // Update the list of completed stages
+        if req.hasUiaaSession,
+           let state = req.uiaaSession?.data.state,
+            let completed = state.completed {
+            for stage in hsResponseData.completed ?? [] {
+                if !completed.contains(stage) {
+                    var session = req.uiaaSession!
+                    session.data["completed"].append(stage)
+                }
+            }
+        }
+        */
+        ourResponseData.completed = req.uiaaSession?.data.state.completed
         
         return ourResponseData.encodeResponse(status: .unauthorized, for: req)
     }
