d24a2fb78ce6e8ccf647b5790dce388bfd71d332
Charles Wright <cvwright@kombucha.systems>
2021-09-07 09:18:45 -0500

    Added App Store receipt validation code from https://github.com/slashmo/swift-app-store-receipt-validation Copyright 2020 Mortiz Lang.  Published under the Apache License, version 2.0.

---
+++ Sources/AppStoreReceiptValidation/Request.swift
@@ -0,0 +1,59 @@
+/*
+ * From https://github.com/slashmo/swift-app-store-receipt-validation
+ * Author: Moritz Lang
+ * License: Apache 2.0
+ */
+
+extension AppStore {
+    enum Environment: String, Codable {
+        case sandbox = "Sandbox"
+        case production = "Production"
+
+        var url: String {
+            switch self {
+            case .sandbox:
+                return "https://sandbox.itunes.apple.com/verifyReceipt"
+            case .production:
+                return "https://buy.itunes.apple.com/verifyReceipt"
+            }
+        }
+    }
+}
+
+extension AppStore {
+    struct Request: Codable {
+        let receiptData: String
+        let password: String?
+        let excludeOldTransactions: Bool?
+
+        enum CodingKeys: String, CodingKey {
+            case receiptData = "receipt-data"
+            case password
+            case excludeOldTransactions = "exclude-old-transactions"
+        }
+    }
+}
+
+extension AppStore {
+    struct Status: Codable {
+        let status: Int
+    }
+
+    struct Response: Codable {
+        let receipt: Receipt // json
+        let latestReceipt: String?
+        let latestReceiptInfo: Receipt? // json
+//    let latestExpiredReceiptInfo: Any? // json
+//    let pendingRenewalInfo: Any?
+        let isRetryable: Bool?
+        let environment: Environment
+
+        enum CodingKeys: String, CodingKey {
+            case receipt
+            case latestReceipt = "latest_receipt"
+            case latestReceiptInfo = "latest_receipt_info"
+            case isRetryable = "is-retryable"
+            case environment
+        }
+    }
+}
