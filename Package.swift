0ee25ee3dc8d42c044795482640470ab6fda74db
Charles Wright <cvwright@kombucha.systems>
2021-09-09 14:05:37 +0000

    Fixed our compile error by upgrading Vapor and removing deps that are no longer necessary

--- Package.swift
+++ Package.swift
@@ -8,14 +8,14 @@ let package = Package(
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.48.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
         .package(url: "https://github.com/nodes-vapor/gatekeeper.git", from: "4.0.0"),
         // For https://github.com/slashmo/swift-app-store-receipt-validation
-        .package(url: "https://github.com/swift-server/async-http-client", .upToNextMajor(from: "1.1.0")),
-        .package(url: "https://github.com/apple/swift-nio", .upToNextMajor(from: "2.14.0")),
+        //.package(url: "https://github.com/swift-server/async-http-client", .upToNextMajor(from: "1.1.0")),
+        //.package(url: "https://github.com/apple/swift-nio", .upToNextMajor(from: "2.14.0")),
         // I got tired of trying to make Vapor expose its underlying Swift NIO bits -- Ultimately it's easier to re-write the receipt validation package's Client class to just use Vapor directly.  smh.
         //.package(url: "https://github.com/slashmo/swift-app-store-receipt-validation", .upToNextMajor(from: "0.1.0")),
     ],
@@ -29,8 +29,8 @@ let package = Package(
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "Gatekeeper", package: "gatekeeper"),
                 // For https://github.com/slashmo/swift-app-store-receipt-validation
-                .product(name: "NIO", package: "swift-nio"),
-                .product(name: "AsyncHTTPClient", package: "async-http-client"),
+                //.product(name: "NIO", package: "swift-nio"),
+                //.product(name: "AsyncHTTPClient", package: "async-http-client"),
                 //.product(name: "AppStoreReceiptValidation", package: "swift-app-store-receipt-validation"),
             ],
             swiftSettings: [
